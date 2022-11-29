f4234691076ac84eac339853e8878acaad838f3e
Liam R Mitchell <liam.mitchell@uleth.ca>
2022-11-29 00:50:37 +0000

    nice kmer data extractor setup, 5 min per f5 file.

--- mbdmbd.py
+++ mbdmbd.py
@@ -28,9 +28,10 @@ def get_args(argv):
     ### req
     required_args.add_argument('--dirA', dest='f5dirA', required=True, help='directory of fast5 files from sample A')
     required_args.add_argument('--dirB', dest='f5dirB', required=True, help='directory of fast5 files from sample B')
-    required_args.add_argument('--output', dest='outputDir', required=True, help='where the output files are to be stored')
+    required_args.add_argument('--output', dest='outDir', required=True, help='where the output files are to be stored')
     # optional
     optional_args.add_argument('--verbose', dest='verbose', action='store_true', help='call this to make program loud')
+    optional_args.add_argument('--targetbase', dest='targetBase', default='A', help='you can change which base is in the middle of NN.NN kmer motifs that we compare, default A')
     ##########
     parser._action_groups.append(optional_args)
     parser.set_defaults()
@@ -43,11 +44,6 @@ def extract_f5_data(f, rn):
     # return trace where move == 1, so we only get basecalls present in the seq
     return sequence.replace("U","T"), trace[move==1]
 
-# https://towardsdatascience.com/gaussian-mixture-models-explained-6986aaf5a95
-# https://github.com/ocontreras309/ML_Notebooks/blob/master/GMM_Implementation.ipynb
-### go all the way to the bottom
-# https://cmdlinetips.com/2021/03/gaussian-mixture-models-with-scikit-learn-in-python/amp/
-
 #def main(argv=sys.argv):
 if True:
     argv=sys.argv
@@ -56,38 +52,37 @@ if True:
     # set bars verbosity 
     tqdm.pandas(disable = not args.verbose)
     ### output storage
-    if not os.path.exists(args.outputDir):
-        os.mkdir(args.outputDir)
+    if not os.path.exists(args.outDir):
+        os.mkdir(args.outDir)
     ########################################
     ############ iterate f5 file ###########
     ########################################
-    big_sig = []
+    big_list = []
     for f5file in tqdm(glob.glob(os.path.join(args.f5dirA,"*"))):
         f = h5py.File(f5file, 'r')
         for rn in f.keys():
             seq_all, sig_all = extract_f5_data(f, rn)
-            seq_idx = ['A' == b for b in seq_all]
-            seq = [seq_all[i-2:i+3] for i in np.where(seq_idx)[0]]
-            sig = [sig_all[i-2:i+3] for i in np.where(seq_idx)[0]]
-            # removing truncated kmers
-            sig = [sig[i] for i in np.where([len(s) == 5 for s in sig])[0]]
-            big_sig.append(sig) 
-    # with single bases:
-    # meaning signal axis 1 gives mean per base, axis 0 gives 5 mean voltages for all base
-    # with kmer:
-    # mean signal axis 1 gives 8 voltage channel mean per kmer
-    # mean signal axis 0 gives 5x8 mean for whole set
-    # meaning again axis 1 on 1 gives one mean value per kmer
+            seq_idx = [args.targetBase == b for b in seq_all]
+            iw = np.where(seq_idx)[0]
+            ## vector of individual kmer seq/sig pairs
+            seq = [seq_all[i-2:i+3] for i in iw]
+            sig = [sig_all[i-2:i+3] for i in iw]
+            # remove truncated
+            iw2 = np.where([len(s) == 5 for s in seq])[0]
+            # check
+            if len(seq) != len(sig):
+                print("AVAST AND CURSES, ME VECTORS' LENGTHS ARE ASKEW")
+            # make serieses
+            big_list.append(pd.DataFrame([pd.Series(data={'signal':sig[i],'kmer':seq[i],'read_name':rn,'seqloc':str(iw[i])}) for i in iw2]))
+    
+    kmer_table = pd.concat(big_list)
+    del big_list
+    exit()
+
     X = np.mean(np.mean(np.concatenate(big_sig), axis=1), axis=1)
     plot = sns.histplot(X, bins=50)
     fig = plot.get_figure()
-    fig.savefig("test.png")
-
-    exit()
-    n_clusters = 2
-    gmm = GaussianMixture(n_components=n_clusters, max_iter=100).fit(X)
-    gmm_scores = gmm.score_samples(X)
-    print("mean: {}\n score: {}\n".format(gmm.means_, gmm_scores))
+    fig.savefig(os.path.join(args.outDir, "big-test.png"))
 
 #if __name__=="__main__":
 #    main(sys.argv)
--- run_py.sh
+++ run_py.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-f5a="/storage/liam/re-basecall/p48_anti-adar_guppy6.3/fast5_pass"
-f5b="/storage/liam/re-basecall/p50_Scramble_Si_guppy6.3/fast5_pass"
-#f5a="/storage/liam/secret-project/mbdmbd/data-files/f5_antiadar/"
-#f5b="/storage/liam/secret-project/mbdmbd/data-files/f5_scramble/"
+#f5a="/storage/liam/re-basecall/p48_anti-adar_guppy6.3/fast5_pass"
+#f5b="/storage/liam/re-basecall/p50_Scramble_Si_guppy6.3/fast5_pass"
+f5a="/storage/liam/secret-project/mbdmbd/data-files/f5_antiadar/"
+f5b="/storage/liam/secret-project/mbdmbd/data-files/f5_scramble/"
 
 python -i mbdmbd.py --dirA $f5a --dirB $f5b --output "./pyout" --verbose
